198800fe4a08b7ab1171dcc7172111a2343912bd
omeldar <eldar.work.edu@gmail.com>
2026-02-27 13:48:39 +0100

    Remove nix flake app

--- docs/flake.nix
+++ docs/flake.nix
@@ -8,8 +8,6 @@
     systems = [ "x86_64-linux" "aarch64-linux" ];
     forAll = f: builtins.listToAttrs (map (s: { name = s; value = f s; }) systems);
   in {
-
-    # Development shell
     devShells = forAll (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -30,25 +28,9 @@
 
             alias watch-build="find . -name '*.tex' -o -name 'Makefile' | entr -c make"
 
-            echo "-------------------------------------------------------"
             echo "Manual build: make"
             echo "Auto build:   watch-build"
-            echo "-------------------------------------------------------"
           '';
-        };
-      });
-
-    # Clean build entrypoint for CI and local usage
-    apps = forAll (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        build-docs = {
-          type = "app";
-          program = toString (pkgs.writeShellScript "build-docs" ''
-            cd ${self}
-            make
-          '');
         };
       });
   };
